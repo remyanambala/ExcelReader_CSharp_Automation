b70414d2d758c7e54716ccb0cd58ca3928ee38df
Remya <remya.nambala@yahoo.com.au>
2020-05-31 12:13:08 +1000

    commit

---
+++ ExcelReaderMSTests/ReadExcelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelReaderMSTests
+{
+    [TestClass]
+    public class ExcelReaderMSTests
+    {
+        public static string buildLoc = System.IO.Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+        public static readonly String dataFilePath = Path.GetFullPath(Path.Combine( buildLoc, @"..\..\TestFiles\","TestExcel1.xlsx"));
+      
+
+        String filepath;
+
+        [TestMethod]
+        public void TestReadValueFromExcel1()
+        {
+            ExcelReader.OpenExcel(dataFilePath);
+           Console.WriteLine(ExcelReader.ReadData(dataFilePath,"Purchase",1, "Fname"));
+
+        }
+        public void TestReadValueFromExcel2()
+        {
+        }
+    }
+}
